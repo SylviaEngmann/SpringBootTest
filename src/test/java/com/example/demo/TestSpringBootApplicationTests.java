0f0b24b2d8667f53f356d20f5b7510136e497d01
Sylvia Engmann <d3vg0d355@theMambaMentality.local>
2021-03-15 15:46:59 +0000

    updated

--- src/test/java/com/example/demo/TestSpringBootApplicationTests.java
+++ src/test/java/com/example/demo/TestSpringBootApplicationTests.java
@@ -1,13 +1,17 @@
 package com.example.demo;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Assertions.*;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+//@SpringBootTest
 class TestSpringBootApplicationTests {
 
 	@Test
-	void contextLoads() {
+	void returnInteger() {
+		//assertEquals(5,5);
 	}
+
+
 
 }
