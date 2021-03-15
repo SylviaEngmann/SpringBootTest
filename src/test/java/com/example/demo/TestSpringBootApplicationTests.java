9beba38ffbd1c99a7b60ff7ef89d9a6b8dafe3eb
Sylvia Engmann <d3vg0d355@theMambaMentality.local>
2021-03-15 15:49:12 +0000

    updated

--- src/test/java/com/example/demo/TestSpringBootApplicationTests.java
+++ src/test/java/com/example/demo/TestSpringBootApplicationTests.java
@@ -2,7 +2,7 @@ package com.example.demo;
 
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.Assertions.*;
-import org.springframework.boot.test.context.SpringBootTest;
+//import org.springframework.boot.test.context.SpringBootTest;
 
 //@SpringBootTest
 class TestSpringBootApplicationTests {
