753f9728639120da322a19acd470e18bbf78ee86
Sylvia Engmann <d3vg0d355@theMambaMentality.local>
2021-03-16 16:31:55 +0000

    Heroku deployment files

--- src/test/java/com/example/demo/TestSpringBootApplicationTests.java
+++ src/test/java/com/example/demo/TestSpringBootApplicationTests.java
@@ -1,14 +1,13 @@
 package com.example.demo;
 
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.Assertions.*;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class TestSpringBootApplicationTests {
 
 	@Test
-	void contextLoad() {
+	void contextLoads() {
 	}
 
 
