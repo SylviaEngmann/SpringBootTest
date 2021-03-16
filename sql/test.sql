56244fd97b68adef816824d0eedcaa8c8b9cc4e2
Sylvia Engmann <d3vg0d355@theMambaMentality.local>
2021-03-16 12:02:25 +0000

    Changes for db

---
+++ sql/test.sql
@@ -0,0 +1,12 @@
+create table student
+(
+    id    bigint not null
+        constraint student_pkey
+            primary key,
+    age   integer,
+    dob   date,
+    email varchar(255),
+    name  varchar(255)
+);
+
+create sequence student_sequence;
