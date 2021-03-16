58f1a12f54c38ee827e4bd12b5715946b96e8dc1
Sylvia Engmann <d3vg0d355@theMambaMentality.local>
2021-03-16 15:45:59 +0000

    Changes for db

--- test.sql
+++ test.sql
@@ -1,15 +1,14 @@
-CREATE DATABASE IF NOT EXISTS `student` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
-USE `student`;
+CREATE DATABASE `student`;
 
-create table `student`
+create table student
 (
-    `id`    bigint not null
+    id    bigint not null
         constraint student_pkey
             primary key,
-    `age`   integer,
-    `dob`   date,
-    `email` varchar(255),
-    `name`  varchar(255)
+    age   integer,
+    dob   date,
+    email varchar(255),
+    name  varchar(255)
 );
 
 create sequence student_sequence;
